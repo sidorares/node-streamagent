7b53957270f316f727e3ba35c7e47a75f6ee0537
Andrey Sidorov <sidorares@yandex.ru>
2013-10-18 15:46:53 +1100

    initial import

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ node-streamagent
 ================
 
 Connect http/websockets node.js client using arbitrary duplex stream
+
+```js
+   var net         = require('net');
+   var StreamAgent = require('streamagent');
+   http.request({path: '/some-endpoint', agent: StreamAgent(net.connect('/var/some.sock')), function(res) {
+     // ..
+   }).end();
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+var Socket = require('net').Socket;
+var Agent  = require('http').Agent;
+
+function socketify(stream) {
+  if (stream.constructor === Socket)
+     return;
+  stream.on('data', function(data) {
+    if (stream.ondata)
+      stream.ondta(data, 0, data.length); 
+  });
+  stream.setTimeout = function() {};
+  stream.setNoDelay = function() {};
+}
+
+function StreamAgent(stream) {
+  // Agent expect net.Socket-like stream
+  socketify(stream);    
+
+  var Agent = new Agent();
+  agent.createSocket(req, options) {
+    return stream;
+  }
+  return agent;
+}
+
+module.exports = StreamAgent;
